f992986cf8936f770fc5a8d0ce2810295f2f8a93
Paulius Velesko <pvelesko@pglc.io>
2024-04-25 05:01:27 +0300

    Syncthreads refactor     * Refactor code generation to prevent deadlocks due to syncthread       getting called for threads that are already dead

--- include/ceed/jit-source/hip/hip-ref-basis-nontensor.h
+++ include/ceed/jit-source/hip/hip-ref-basis-nontensor.h
@@ -21,18 +21,18 @@
 //------------------------------------------------------------------------------
 extern "C" __global__ void Interp(const CeedInt num_elem, const CeedScalar *__restrict__ d_B, const CeedScalar *__restrict__ d_U,
                                   CeedScalar *__restrict__ d_V) {
-  for (CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z; elem < num_elem; elem += gridDim.x * blockDim.z) {
+    CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z;
+    if (elem < num_elem)
     Contract<BASIS_NUM_COMP, BASIS_Q_COMP_INTERP, BASIS_P, BASIS_Q>(elem, BASIS_P, BASIS_Q, BASIS_P * num_elem, BASIS_Q * num_elem,
                                                                     BASIS_NUM_COMP * BASIS_Q * num_elem, d_B, d_U, d_V);
-  }
 }
 
 extern "C" __global__ void InterpTranspose(const CeedInt num_elem, const CeedScalar *__restrict__ d_B, const CeedScalar *__restrict__ d_U,
                                            CeedScalar *__restrict__ d_V) {
-  for (CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z; elem < num_elem; elem += gridDim.x * blockDim.z) {
+        CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z;
+    if (elem < num_elem)
     ContractTranspose<BASIS_NUM_COMP, BASIS_Q_COMP_INTERP, BASIS_P, BASIS_Q>(elem, BASIS_Q, BASIS_P, BASIS_Q * num_elem, BASIS_P * num_elem,
                                                                              BASIS_NUM_COMP * BASIS_Q * num_elem, d_B, d_U, d_V);
-  }
 }
 
 //------------------------------------------------------------------------------
@@ -40,18 +40,18 @@ extern "C" __global__ void InterpTranspose(const CeedInt num_elem, const CeedSca
 //------------------------------------------------------------------------------
 extern "C" __global__ void Deriv(const CeedInt num_elem, const CeedScalar *__restrict__ d_B, const CeedScalar *__restrict__ d_U,
                                  CeedScalar *__restrict__ d_V) {
-  for (CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z; elem < num_elem; elem += gridDim.x * blockDim.z) {
+     CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z;
+    if (elem < num_elem) 
     Contract<BASIS_NUM_COMP, BASIS_Q_COMP_DERIV, BASIS_P, BASIS_Q>(elem, BASIS_P, BASIS_Q, BASIS_P * num_elem, BASIS_Q * num_elem,
                                                                    BASIS_NUM_COMP * BASIS_Q * num_elem, d_B, d_U, d_V);
-  }
 }
 
 extern "C" __global__ void DerivTranspose(const CeedInt num_elem, const CeedScalar *__restrict__ d_B, const CeedScalar *__restrict__ d_U,
                                           CeedScalar *__restrict__ d_V) {
-  for (CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z; elem < num_elem; elem += gridDim.x * blockDim.z) {
+      CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z;
+    if (elem < num_elem)
     ContractTranspose<BASIS_NUM_COMP, BASIS_Q_COMP_DERIV, BASIS_P, BASIS_Q>(elem, BASIS_Q, BASIS_P, BASIS_Q * num_elem, BASIS_P * num_elem,
                                                                             BASIS_NUM_COMP * BASIS_Q * num_elem, d_B, d_U, d_V);
-  }
 }
 
 //------------------------------------------------------------------------------
@@ -61,7 +61,7 @@ extern "C" __global__ void Weight(const CeedInt num_elem, const CeedScalar *__re
   const CeedInt t_id = threadIdx.x;
   // TODO load q_weight in shared memory if blockDim.z > 1?
 
-  for (CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z; elem < num_elem; elem += gridDim.x * blockDim.z) {
+     CeedInt elem = blockIdx.x * blockDim.z + threadIdx.z;
+    if (elem < num_elem) 
     d_V[elem * BASIS_Q + t_id] = q_weight[t_id];
-  }
 }
